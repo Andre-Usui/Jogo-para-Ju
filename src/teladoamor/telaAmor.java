8b80dae4ee50d1ddc90be42d62d65220beec2505
Andre-Usui <prof.andreusui@gmail.com>
2022-04-17 09:39:09 -0300

    adicionando novas categorias

--- src/teladoamor/telaAmor.java
+++ src/teladoamor/telaAmor.java
@@ -10,24 +10,29 @@ package teladoamor;
  */
 public class telaAmor extends javax.swing.JFrame {
 
-    /* Este projeto é um treinamento utilizando o máximo de
-     * funções possível de maneira eficaz e limpa uma decla- 
+    /*
+     * Este projeto é um treinamento utilizando o máximo de
+     * funções possível de maneira eficaz e limpa uma decla-
      * ção de amor à minha esposa.
      * 
      * No momento, estou tentando fazer um contador para que
      * toda vez que a ação 'txtAmorActionPerformed' for acio-
      * nada conte no int c e que após c > 3 será mandada uma
-     * nova mensagem. Mas c só é somado uma única vez. 
+     * nova mensagem. Mas c só é somado uma única vez.
      *
      * Tentando transformar 'txtAmorActionPerformed' em um ob-
-     * jeto para verificar se o erro é solucionado. 
-    */ 
+     * jeto para verificar se o erro é solucionado. ~ não funcionou
+     * 
+     * Tentando transformar 'txtAmorActionPerformed' em um método
+     * static, porém o IDE NETBEANS não permite as alterações.
+     */
     public telaAmor() {
         initComponents();
     }
 
     @SuppressWarnings("unchecked")
-    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    // <editor-fold defaultstate="collapsed" desc="Generated
+    // Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
         lblImagem = new javax.swing.JLabel();
@@ -65,92 +70,105 @@ public class telaAmor extends javax.swing.JFrame {
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addGroup(layout.createSequentialGroup()
-                                .addContainerGap()
-                                .addComponent(lblImagem, javax.swing.GroupLayout.PREFERRED_SIZE, 256, javax.swing.GroupLayout.PREFERRED_SIZE))
-                            .addGroup(layout.createSequentialGroup()
-                                .addGap(74, 74, 74)
-                                .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 162, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                            .addComponent(jLabel2, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                            .addComponent(lblMensagem, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                            .addGroup(layout.createSequentialGroup()
-                                .addComponent(txtAmor, javax.swing.GroupLayout.PREFERRED_SIZE, 250, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addGap(0, 49, Short.MAX_VALUE))))
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                        .addGap(0, 0, Short.MAX_VALUE)
-                        .addComponent(lblContagem)))
-                .addContainerGap())
-        );
+                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGroup(layout.createSequentialGroup()
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                        .addGroup(layout.createSequentialGroup()
+                                                .addGroup(layout
+                                                        .createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                                        .addGroup(layout.createSequentialGroup()
+                                                                .addContainerGap()
+                                                                .addComponent(lblImagem,
+                                                                        javax.swing.GroupLayout.PREFERRED_SIZE, 256,
+                                                                        javax.swing.GroupLayout.PREFERRED_SIZE))
+                                                        .addGroup(layout.createSequentialGroup()
+                                                                .addGap(74, 74, 74)
+                                                                .addComponent(jLabel3,
+                                                                        javax.swing.GroupLayout.PREFERRED_SIZE, 162,
+                                                                        javax.swing.GroupLayout.PREFERRED_SIZE)))
+                                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                                .addGroup(layout
+                                                        .createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                                        .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE,
+                                                                javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                                        .addComponent(jLabel2,
+                                                                javax.swing.GroupLayout.Alignment.TRAILING,
+                                                                javax.swing.GroupLayout.DEFAULT_SIZE,
+                                                                javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                                        .addComponent(lblMensagem, javax.swing.GroupLayout.DEFAULT_SIZE,
+                                                                javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                                        .addGroup(layout.createSequentialGroup()
+                                                                .addComponent(txtAmor,
+                                                                        javax.swing.GroupLayout.PREFERRED_SIZE, 250,
+                                                                        javax.swing.GroupLayout.PREFERRED_SIZE)
+                                                                .addGap(0, 49, Short.MAX_VALUE))))
+                                        .addGroup(javax.swing.GroupLayout.Alignment.TRAILING,
+                                                layout.createSequentialGroup()
+                                                        .addGap(0, 0, Short.MAX_VALUE)
+                                                        .addComponent(lblContagem)))
+                                .addContainerGap()));
         layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addContainerGap()
-                        .addComponent(lblImagem, javax.swing.GroupLayout.PREFERRED_SIZE, 256, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(60, 60, 60)
-                        .addComponent(jLabel1)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jLabel2)
-                        .addGap(46, 46, 46)
-                        .addComponent(lblMensagem, javax.swing.GroupLayout.PREFERRED_SIZE, 98, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addGap(26, 26, 26)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel3)
-                    .addComponent(txtAmor, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 29, Short.MAX_VALUE)
-                .addComponent(lblContagem)
-                .addGap(25, 25, 25))
-        );
+                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGroup(layout.createSequentialGroup()
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                        .addGroup(layout.createSequentialGroup()
+                                                .addContainerGap()
+                                                .addComponent(lblImagem, javax.swing.GroupLayout.PREFERRED_SIZE, 256,
+                                                        javax.swing.GroupLayout.PREFERRED_SIZE))
+                                        .addGroup(layout.createSequentialGroup()
+                                                .addGap(60, 60, 60)
+                                                .addComponent(jLabel1)
+                                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                                .addComponent(jLabel2)
+                                                .addGap(46, 46, 46)
+                                                .addComponent(lblMensagem, javax.swing.GroupLayout.PREFERRED_SIZE, 98,
+                                                        javax.swing.GroupLayout.PREFERRED_SIZE)))
+                                .addGap(26, 26, 26)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                                        .addComponent(jLabel3)
+                                        .addComponent(txtAmor, javax.swing.GroupLayout.PREFERRED_SIZE,
+                                                javax.swing.GroupLayout.DEFAULT_SIZE,
+                                                javax.swing.GroupLayout.PREFERRED_SIZE))
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 29,
+                                        Short.MAX_VALUE)
+                                .addComponent(lblContagem)
+                                .addGap(25, 25, 25)));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void txtAmorActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtAmorActionPerformed
+    private void txtAmorActionPerformed(java.awt.event.ActionEvent evt) {// GEN-FIRST:event_txtAmorActionPerformed
         String nome = txtAmor.getText();
         String amor = "Julianny";
         String nunca = "Eduarda";
-        int c = 0;    
-            //for (c = 0; nome.equals(amor) == false; c++);{
-            if (nome.equals(amor)) {
-                lblImagem.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagens/i love you.png")));
-                lblMensagem.setText("André te ama que só a porra");
-            } else if (nome.equals(nunca)) {
-                lblImagem.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagens/no love symbol.png")));
-                lblMensagem.setText("Tas viajando né kkkkk");
-            } else if (nome.equals(amor) == false) {                    
-                    lblContagem.setText("Você errou " + c + " vezes.");
-                    lblImagem.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagens/no love symbol.png")));
-                    lblMensagem.setText("André não te ama");
-            }           
-     
-                
-                
-               
-               
-     
-            
-        
-        
-    }//GEN-LAST:event_txtAmorActionPerformed
+        int c = 0;
+        // for (c = 0; nome.equals(amor) == false; c++);{
+        if (nome.equals(amor)) {
+            lblImagem.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagens/i love you.png")));
+            lblMensagem.setText("André te ama que só a porra");
+        } else if (nome.equals(nunca)) {
+            lblImagem.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagens/no love symbol.png")));
+            lblMensagem.setText("Tas viajando né kkkkk");
+        } else if (nome.equals(amor) == false) {
+            lblContagem.setText("Você errou " + c + " vezes.");
+            lblImagem.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagens/no love symbol.png")));
+            lblMensagem.setText("André não te ama");
+        }
+
+    }// GEN-LAST:event_txtAmorActionPerformed
 
     /**
      * @param args the command line arguments
      */
     public static void main(String args[]) {
         /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+        // <editor-fold defaultstate="collapsed" desc=" Look and feel setting code
+        // (optional) ">
+        /*
+         * If Nimbus (introduced in Java SE 6) is not available, stay with the default
+         * look and feel.
+         * For details see
+         * http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html
          */
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
@@ -168,7 +186,7 @@ public class telaAmor extends javax.swing.JFrame {
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
             java.util.logging.Logger.getLogger(telaAmor.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
-        //</editor-fold>
+        // </editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
